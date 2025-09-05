9ff7dd5efc6c8f04d0c3f1a82bb3423c0f008047
Tiltedddddd <darren7172@gmail.com>
2025-09-05 13:08:27 +0800

    First iteration of responsive navbar done.

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+// Sidebar Menu Logic
+function openNav() {
+  document.getElementById("sideNav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("sideNav").style.width = "0";
+}
