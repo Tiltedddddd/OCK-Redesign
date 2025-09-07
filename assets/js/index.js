bc6e5cf4f33e75ab82c685b216355c03c684a410
Tiltedddddd <darren7172@gmail.com>
2025-09-07 22:45:21 +0800

    Responsive navbar done. Base features completed: Navbar collapses into hamburger and mini logo icon, Cant scroll when sideNav is open, escape closes the sideNav, added a overlay div to dim the background when the sideNav is open, made tapping the overlay layer close the sideNav, made colours more pleasing + following the website theme.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,25 @@
 // Sidebar Menu Logic
 function openNav() {
   document.getElementById("sideNav").style.width = "250px";
+  document.getElementById("sideNavOverlay").style.display = "block";
+  document.getElementById("sideNavOverlay").style.zIndex = "1";
+
+  // Disable page scrolling when sideNav is open  
+  document.body.style.overflow = "hidden";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("sideNav").style.width = "0";
+  document.getElementById("sideNavOverlay").style.display = "none";
+
+  // Re-enable scrolling
+  document.body.style.overflow = "auto";
 }
+
+// Makes sure esc closes the navbar for uiux 
+document.addEventListener("keydown", function(e) {
+    if (e.key === "Escape") {
+      closeNav();
+    }
+  });
